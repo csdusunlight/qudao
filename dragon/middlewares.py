79be89693e886853c7abfe53e049f4edc04639da
lch <690501772@qq.com>
2017-09-20 09:28:09 +0800

    mid

--- dragon/middlewares.py
+++ dragon/middlewares.py
@@ -10,5 +10,8 @@ class SubdomainMiddleware(object):
         domain_parts = request.get_host().split('.')
         if len(domain_parts) == 3:
             username = domain_parts[0]
-            request.user = MyUser.objects.get(username=username)
+            try:
+                request.user = MyUser.objects.get(username=username)
+            except:
+                pass
         return None

bcfa25927186d4c5369ed878725580eb92f69a12
lvchunhui7 <lvchunhui7@126.com>
2017-09-26 14:56:17 +0800

    export

--- account/urls.py
+++ account/urls.py
@@ -46,7 +46,6 @@ urlpatterns = [
     url(r'^account_setting/$', 'account.views.account_setting', name='account_setting'),
     url(r'^account_submit/$', 'account.channel.channel', name='account_submit'),
     url(r'^account_audited/$', 'account.views.account_audited', name='account_audited'),
-    url(r'^account_audited/export_finance_excel/$', 'account.channel.export_audit_result', ),
 
     url(r'^project/$', 'account.views.project_manage', name='project_manage'),
     url(r'^project_create/$', 'account.views.project_create', name='project_create'),

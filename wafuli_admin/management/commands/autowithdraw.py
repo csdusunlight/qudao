b827b11cd223a027d6b4e24b4d7f0cf9025a158b
lvchunhui7 <lvchunhui7@126.com>
2017-09-30 16:18:32 +0800

    autowith

--- wafuli_admin/management/commands/autowithdraw.py
+++ wafuli_admin/management/commands/autowithdraw.py
@@ -11,18 +11,19 @@ from django.core.management.base import BaseCommand
 from account.models import MyUser
 from account.transaction import charge_money
 from wafuli.models import WithdrawLog
+from django.db import transaction
 logger = logging.getLogger("wafuli")
 class Command(BaseCommand):
     def handle(self, *args, **options):
         logger.info("******Auto-withdrawing is beginning*********")
         begin_time = time.time()
         channels = MyUser.objects.filter(balance__gte=10)
-        for ch in channels:
-            user = ch.user
+        for user in channels:
             card = user.user_bankcard.first()
             if not card:
                 continue
-            charge_money(user, '1', user.balance, u'系统自动提现')
-            WithdrawLog.objects.create(user=user, amount=user.balance, audit_state='1')
+            with transaction.atomic():
+                charge_money(user, '1', user.balance, u'系统自动提现')
+                WithdrawLog.objects.create(user=user, amount=user.balance, audit_state='1')
         end_time = time.time()
         logger.info("******Auto-withdrawing is finished, time:%s*********",end_time-begin_time)

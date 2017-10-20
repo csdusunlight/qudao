a9c3837880d0b746da8649d62edc2070eaa0e32f
lvchunhui7 <lvchunhui7@126.com>
2017-10-20 18:40:35 +0800

    export

--- account/channel.py
+++ account/channel.py
@@ -220,17 +220,18 @@ def export_investlog(request):
     audittime_0 = request.GET.get("audittime_0", None)
     audittime_1 = request.GET.get("audittime_1", None)
     state = request.GET.get("audit_state",'')
+    print submittime_0,submittime_0
     if not submittime_0 or not submittime_1:
         raise Http404
     s = datetime.datetime.strptime(submittime_0,'%Y-%m-%d')
     e = datetime.datetime.strptime(submittime_1,'%Y-%m-%d')
+    e += timedelta(days=1)
     if (e - s).days > 5:
         raise Http404
     item_list = item_list.filter( submit_time__range=(s,e))
     if investtime_0 and investtime_1:
         s = datetime.datetime.strptime(investtime_0,'%Y-%m-%d')
         e = datetime.datetime.strptime(investtime_1,'%Y-%m-%d')
-        e += timedelta(days=1)
         item_list = item_list.filter(invest_date__range=(s,e))
     if audittime_0 and audittime_1:
         s = datetime.datetime.strptime(audittime_0,'%Y-%m-%d')
@@ -266,6 +267,7 @@ def export_investlog(request):
     zhifubao = request.GET.get("zhifubao", None)
     if zhifubao:
         item_list = item_list.filter(zhifubao__contains=zhifubao)
+    print state
     if state:
         item_list = item_list.filter(audit_state=state)
     item_list = item_list.select_related('project').order_by('submit_time')

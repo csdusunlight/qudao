67e6091f1a4a3d4b725374574209d7808977fbaa
lvchunhui7 <lvchunhui7@126.com>
2017-11-16 16:06:25 +0800

    views

--- docs/views.py
+++ docs/views.py
@@ -10,10 +10,10 @@ def get_user_doc_list(request):
     return render(request, 'doclist.html', {'docs':docs})
 
 @login_required
-def create_doc(request):
-    return render(request, 'create_update_doc.html',)
-
-@login_required
-def update_doc(request, id):
-    doc = Document.objects.get(user=request.user, id=id)
+def create_or_update_doc(request, id=None):
+    if id:
+        doc = Document.objects.get(user=request.user, id=id)
+    else:
+        doc = Document.objects.create(user=request.user)
     return render(request, 'create_update_doc.html', {'doc':doc})
+

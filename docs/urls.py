000d9bde4bedbe3ccc74861adebc4a6696f5186e
lvchunhui7 <lvchunhui7@126.com>
2017-11-20 15:41:50 +0800

    docs

--- docs/urls.py
+++ docs/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^list/$', views.get_user_doc_list, name='doc-list'),
     url(r'^create/$', views.create_doc, name='create_doc'),
     url(r'^(?P<id>[0-9a-zA-Z]+)/$', views.update_doc, name='update_doc'),
-    url(r'^duplicate/(?P<id>[0-9a-zA-Z]+)/$', views.duplicate_doc, name='update_doc'),
+    url(r'^duplicate/(?P<id>[0-9a-zA-Z]+)/$', views.duplicate_doc, name='duplicate_doc'),
 ]

9a92d11e84b48296c68288b81a16733346ecfab1
lvchunhui7 <lvchunhui7@126.com>
2017-11-23 19:22:41 +0800

    wexin_bind

--- weixin/settings.py
+++ weixin/settings.py
@@ -4,5 +4,5 @@ Created on 20171123
 
 @author: lch
 '''
-book_invest_notify_templateid = 'OPENTM413481557'
-submit_investlog_notify_templateid = 'OPENTM413480991'
+book_invest_notify_templateid = 'sRZ_AB9KsDOS6JTZDqXvbK52lJxLxITMsaSXzFzZFxI'
+submit_investlog_notify_templateid = 'mvxElCXttPBbrbF5Rqt6odqx_pGTxq4ntgqLu_KNE4k'

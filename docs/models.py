13385bf8bf5a59e4aafb48ca483b0f88e634617a
lvchunhui7 <lvchunhui7@126.com>
2017-12-07 10:50:08 +0800

    rest add url

--- docs/models.py
+++ docs/models.py
@@ -13,7 +13,7 @@ class Document(RandomPrimaryIdModel):
     view_count = models.IntegerField(u"浏览次数", default=0)
     is_on = models.BooleanField(u"开启/关闭", default=True)
     secret = models.CharField(u"密码", max_length=20, blank=True)
-    is_star = models.BooleanField(u"标签", blank=False)
+    is_star = models.BooleanField(u"标签", default=False)
     def __unicode__(self):
         return self.user.username + self.title
     def fanshu_url(self):

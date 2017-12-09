2380da5afdc591c0bd264950b3763c175bcac944
lvchunhui7 <lvchunhui7@126.com>
2017-12-09 14:32:43 +0800

    weixin

--- weixin/views.py
+++ weixin/views.py
@@ -81,7 +81,7 @@ def bind_user(request):
     else:
         code = request.GET.get('code','')
         to_url = request.GET.get('to_url')
-        logger.error(to_url)
+        logger.error(str(to_url)+str(code))
         if not code:
             return HttpResponse(u"微信授权失败，请稍后再试")
         url = ' https://api.weixin.qq.com/sns/oauth2/access_token'

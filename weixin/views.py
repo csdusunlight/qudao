a5562b1ac7530856e70028f009f052297006dfdb
lvchunhui7 <lvchunhui7@126.com>
2017-12-27 14:43:53 +0800

    weixin autoreply

--- weixin/views.py
+++ weixin/views.py
@@ -136,4 +136,85 @@ def sendWeixinTemplate():
         kwarg.update(data=data, touser=openid)
         ret = httpconn(url, kwarg, 1)
         logger.info(ret)
-    
+ 
+
+import xml.etree.ElementTree as ET
+def autoreply(request):
+    try:
+        webData = request.body
+        xmlData = ET.fromstring(webData)
+
+        msg_type = xmlData.find('MsgType').text
+        ToUserName = xmlData.find('ToUserName').text
+        FromUserName = xmlData.find('FromUserName').text
+        CreateTime = xmlData.find('CreateTime').text
+        MsgType = xmlData.find('MsgType').text
+        MsgId = xmlData.find('MsgId').text
+
+        toUser = FromUserName
+        fromUser = ToUserName
+
+        if msg_type == 'text':
+            content = "您好,欢迎来到Python大学习!希望我们可以一起进步!"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            print "成功了!!!!!!!!!!!!!!!!!!!"
+            print replyMsg
+            return replyMsg.send()
+
+        elif msg_type == 'image':
+            content = "图片已收到,谢谢"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+        elif msg_type == 'voice':
+            content = "语音已收到,谢谢"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+        elif msg_type == 'video':
+            content = "视频已收到,谢谢"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+        elif msg_type == 'shortvideo':
+            content = "小视频已收到,谢谢"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+        elif msg_type == 'location':
+            content = "位置已收到,谢谢"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+        else:
+            msg_type == 'link'
+            content = "链接已收到,谢谢"
+            replyMsg = TextMsg(toUser, fromUser, content)
+            return replyMsg.send()
+
+    except Exception, Argment:
+        return Argment
+
+class Msg(object):
+    def __init__(self, xmlData):
+        self.ToUserName = xmlData.find('ToUserName').text
+        self.FromUserName = xmlData.find('FromUserName').text
+        self.CreateTime = xmlData.find('CreateTime').text
+        self.MsgType = xmlData.find('MsgType').text
+        self.MsgId = xmlData.find('MsgId').text
+
+import time
+class TextMsg(Msg):
+    def __init__(self, toUserName, fromUserName, content):
+        self.__dict = dict()
+        self.__dict['ToUserName'] = toUserName
+        self.__dict['FromUserName'] = fromUserName
+        self.__dict['CreateTime'] = int(time.time())
+        self.__dict['Content'] = content
+
+    def send(self):
+        XmlForm = """
+        <xml>
+        <ToUserName><![CDATA[{ToUserName}]]></ToUserName>
+        <FromUserName><![CDATA[{FromUserName}]]></FromUserName>
+        <CreateTime>{CreateTime}</CreateTime>
+        <MsgType><![CDATA[text]]></MsgType>
+        <Content><![CDATA[{Content}]]></Content>
+        </xml>
+        """
+        return XmlForm.format(**self.__dict)    

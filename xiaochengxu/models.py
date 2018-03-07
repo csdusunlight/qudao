242d01ae4abc4c1cfc8936aba74c1e04b3060e3e
lvchunhui7 <lvchunhui7@126.com>
2018-03-07 17:42:29 +0800

    xcx

--- xiaochengxu/models.py
+++ xiaochengxu/models.py
@@ -10,15 +10,16 @@ from decimal import Decimal
 from account.models import MyUser
 
 class App(models.Model):
-    wxuser = models.ForeignKey(MyUser, related_name="apps")
+    user = models.ForeignKey(MyUser, related_name="apps")
     app_id = models.CharField(max_length=18, primary_key=True)
     app_secret = models.CharField(max_length=32)
-    app_name = models.CharField(max_length=32) 
+    app_name = models.CharField(max_length=32)
     def __unicode__(self):
         return '%s:%s' % (self.app_name, self.app_id)
 class WXUser(models.Model):
     openid = models.CharField(u'公众号关注者编号', max_length=30, unique=True)
-    app = models.ForeignKey('App', related_name="users")
+    app = models.ForeignKey(App, related_name="wxusers")
+    user = models.ForeignKey(MyUser, related_name="wxusers")
     nickName = models.CharField(u'微信昵称', max_length=30)
     date_joined = models.DateTimeField(u'注册时间', default=timezone.now)
     gender = models.CharField(u'性别', max_length=1)
@@ -30,6 +31,9 @@ class WXUser(models.Model):
     unionid = models.CharField(u'unionid', max_length=50,)
     balance = models.DecimalField(u'账户余额', default = Decimal(0), max_digits=10, decimal_places=2)
     zhifubao = models.CharField(u'收款信息', max_length=64, blank=True)
+    mobile = models.CharField('mobile number', max_length=11)
+    qq_number = models.CharField(u"QQ号", max_length=20)
+    qq_name = models.CharField(u"QQ昵称", max_length=20)
     class Meta:
         verbose_name = 'wxuser'
         verbose_name_plural = 'wxusers'

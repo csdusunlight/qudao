ad5f817dd3df23ac883fe726874a3e643446c535
lvchunhui7 <lvchunhui7@126.com>
2018-05-12 11:23:02 +0800

    alter

--- docs/models.py
+++ docs/models.py
@@ -9,7 +9,7 @@ import datetime
 class Document(RandomPrimaryIdModel):
     user = models.ForeignKey(MyUser, related_name="docs")
     title = models.CharField(u"标题", max_length=20, default=u"无标题")
-    content = models.CharField(u"正文", max_length=100000)
+    content = models.CharField(u"正文", max_length=1000000)
     update_time = models.DateTimeField(u"更新时间", auto_now=True)
     view_count = models.IntegerField(u"浏览次数", default=0)
     is_on = models.BooleanField(u"开启/关闭", default=True)

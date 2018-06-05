85d021185e7c773f496ca90514293951289c2a7e
jiangzongyang <jiangzongyang197@163.com>
2018-06-05 18:07:26 +0800

    add finance

--- restapi/serializers.py
+++ restapi/serializers.py
@@ -64,6 +64,7 @@ class InvestLogSerializer(serializers.ModelSerializer):
     user_level = serializers.CharField(source='user.level', read_only=True)
     user_mobile = serializers.CharField(source='user.mobile', read_only=True)
     audit_state_des = serializers.CharField(source='get_audit_state_display', read_only=True)
+    pay_state_des = serializers.CharField(source='get_pay_state_display', read_only=True)
     preaudit_state_des = serializers.CharField(source='get_preaudit_state_display', read_only=True)
     admin_user = serializers.CharField(source='admin_user.username', read_only=True) 
     admin_user = serializers.CharField(source='admin_user.username', read_only=True)

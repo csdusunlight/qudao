7f8e50c715cb5db8dace344dfe2ea7c9b8f9fa85
lvchunhui7 <lvchunhui7@126.com>
2018-06-08 10:58:05 +0800

    fixbug

--- account/models.py
+++ account/models.py
@@ -138,12 +138,12 @@ class MyUser(AbstractBaseUser, PermissionsMixin):
     zhifubao_real_name = models.CharField(u"支付宝实名", blank=True, max_length=20)
 
     is_channel = models.CharField(u"是否渠道", choices=IS_CHANNEL, default='1', max_length=2)  #
-    channel_refuse_reason = models.CharField(u"渠道拒绝原因", default=False, null=True, blank=True, max_length=300)  #
-    user_origin = models.CharField(u"用户来源", choices=USER_ORIGIN, default='0', max_length=2)
-    user_exp_year = models.CharField(u"用户经验年限", choices=USER_EXP_YEAR, default='0', max_length=2)
-    user_custom_volumn = models.CharField(u"用户客户体量", choices=USER_CUSTOME_VOLUMN, default='0', max_length=2)
+    channel_refuse_reason = models.CharField(u"渠道拒绝原因", blank=True, max_length=300)  #
+    user_origin = models.CharField(u"用户来源", choices=USER_ORIGIN, max_length=2)
+    user_exp_year = models.CharField(u"用户经验年限", choices=USER_EXP_YEAR, max_length=2)
+    user_custom_volumn = models.CharField(u"用户客户体量", choices=USER_CUSTOME_VOLUMN, max_length=2)
     user_funds_volumn = models.CharField(u"用户资金体量", choices=USER_FUNDS_VOLUMN, default='0', max_length=2)
-    user_invest_orientation = models.CharField(u"用户投资去向", choices=USER_INVEST_ORIENTATION, default='0', max_length=2)
+    user_invest_orientation = models.CharField(u"用户投资去向", choices=USER_INVEST_ORIENTATION, max_length=2)
     user_apply_channel_time = models.DateTimeField(u'申请渠道用户时间',null=True,blank=True)
     user_apply_auditor = models.ForeignKey('self', related_name='auditor',
                                            blank=True, null=True, on_delete=models.SET_NULL, default=None)

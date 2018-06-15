2e4cfbaba14a5a92079f2bc667f8fb9631682cac
lvchunhui7 <lvchunhui7@126.com>
2018-06-15 09:13:51 +0800

    seri

--- coupon/serializers.py
+++ coupon/serializers.py
@@ -21,7 +21,8 @@ class UserCouponSerializer(serializers.ModelSerializer):
     contract_name = serializers.CharField(source='contract.name', read_only=True)
     settle_count = serializers.CharField(source='contract.settle_count', read_only=True)
     settle_amount = serializers.CharField(source='contract.settle_amount', read_only=True)
-    project = serializers.CharField(source='contract.project.title', read_only=True)
+    project_title = serializers.CharField(source='contract.project.title', read_only=True)
+    condtype = serializers.CharField(source='contract.condtype', read_only=True)
     class Meta:
         model = UserCoupon
         fields = '__all__'
